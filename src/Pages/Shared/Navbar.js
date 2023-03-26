71b9339ea1cdeb5ef823f6b5a36094b7028cb7c9
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-03-27 01:31:03 +0600

    nab clre

--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../..//assets/logo_2.png'
 
 const Navbar = () => {
@@ -11,36 +12,22 @@ const Navbar = () => {
                     ' src={Logo} alt="" />
                 </div>
             </div>
-            <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1">
-                    <li><a>Item 1</a></li>
-                    <li tabIndex={0}>
-                        <a>
-                            Parent
-                            <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
-                        </a>
-                        <ul className="p-2">
-                            <li><a>Submenu 1</a></li>
-                            <li><a>Submenu 2</a></li>
-                        </ul>
-                    </li>
-                    <li><a>Item 3</a></li>
-                </ul>
-            </div>
             <div className="navbar-end">
+                <ul className="menu menu-horizontal px-1 hidden lg:flex">
+                    <Link className='text-white'>
+                        <button className="btn btn-outline btn-info text-white">Strudent Login</button>
+
+                    </Link>
+                </ul>
                 <div className="dropdown dropdown-end">
                     <label tabIndex={0} className="btn btn-ghost btn-circle lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
                     </label>
                     <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                        <li>
-                            <a className="justify-between">
-                                Profile
-                                <span className="badge">New</span>
-                            </a>
-                        </li>
-                        <li><a>Settings</a></li>
-                        <li><a>Logout</a></li>
+                        <Link className='text-white'>
+                            <button className="btn btn-outline btn-info text-white">Strudent Login</button>
+
+                        </Link>
                     </ul>
                 </div>
             </div>
@@ -50,3 +37,7 @@ const Navbar = () => {
 };
 
 export default Navbar;
+{/* <a>
+Parent
+<svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
+</a> */}
