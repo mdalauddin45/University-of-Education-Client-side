b00b7497d50b5e759ebc473dc4e98ffe9381fe85
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-03-27 02:23:13 +0600

    archives add

--- src/Pages/Shared/Drawer.js
+++ src/Pages/Shared/Drawer.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import Navbar from './Navbar';
+import logo from '../../assets/logo_2.png';
 
 const Drawer = () => {
+    const desgin = "text-[#666666] text-[14px] hover:bg-[#e79800] hover:text-white px-4 py-3 font-bold"
     return (
         <div className="drawer">
             <input id="my-drawer" type="checkbox" className="drawer-toggle" />
@@ -11,8 +13,15 @@ const Drawer = () => {
             <div className="drawer-side">
                 <label htmlFor="my-drawer" className="drawer-overlay"></label>
                 <ul className="menu p-4 w-80 bg-base-100 text-base-content">
-                    <li><a>Sidebar Item 1</a></li>
-                    <li><a>Sidebar Item 2</a></li>
+                    <img className='h-18 px-5 py-5' src={logo} alt="" />
+                    <li>Search Course</li>
+                    <input type="text" placeholder="Keyword...." className="input w-full max-w-xs" />
+                    <div class="divider"></div>
+                    <h1>Archives</h1>
+                    <li><a>january 2023</a></li>
+                    <li><a>February 2023</a></li>
+                    <li><a>March 2023</a></li>
+                    <li><a>April 2023</a></li>
 
                 </ul>
             </div>
