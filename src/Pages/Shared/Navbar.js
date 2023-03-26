ec346417e6031650f928bba5cc35f1adcaa70534
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-03-27 01:07:36 +0600

    nav add

--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -1,38 +1,11 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import logo from "../../assets/logo_2.png";
-
+import React from 'react';
 
 const Navbar = () => {
     return (
-        <div className="navbar bg-base-100 mx-2">
-            <div className="flex-1">
-                <Link to="/">
-                    <img className="h-12 md:h-16 lg:h-20" src={logo} alt="..." />
-                </Link>
-            </div>
-            <div className="flex-none gap-2">
-                <div className="form-control">
-                    <input type="text" placeholder="Search" className="input input-bordered" />
-
-                </div>
-                <div className="dropdown dropdown-end">
-                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                    </label>
-                    <ul tabIndex={0} className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
-                        <li>
-                            <a className="justify-between">
-                                Profile
-                                <span className="badge">New</span>
-                            </a>
-                        </li>
-                        <li><a>Settings</a></li>
-                        <li><a>Logout</a></li>
-                    </ul>
-                </div>
-            </div>
+        <div>
+            <h1>this is nabvar</h1>
         </div>
     );
 };
+
 export default Navbar;
